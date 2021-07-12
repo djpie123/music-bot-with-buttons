453d3e00dc2b4a10430689f8c10b0530873fa49f
djpie123 <shreyashvijay1710@gmail.com>
2021-07-12 12:40:11 +0530

    done big changes and added my package buttube

--- commands/play.js
+++ commands/play.js
@@ -1,10 +1,7 @@
 module.exports.run = async (bot, message, args) => {
     if (!message.member.voice.channel) return message.channel.send('You must be in a voice channel to use this command.');
   
-    const music = args.join(" ");
-
-    bot.distube.play(message, music)
-    message.delete()
+    bot.buttube.play(message, args.join(" "));
 }
 
 module.exports.config = {
